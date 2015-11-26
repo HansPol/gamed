e1cdc24223e16bdfeb1a93adbdd36545540ca3b5
MiauwMaster <putm1500@student.nhl.nl>
2015-11-26 10:08:05 +0100

    Spawning update
    Spawining van meerdere objecten tegelijk werkt aleen worden ze soms nog
    op elkaar gelegd.

--- Assets/Scripts/Spawning.cs
+++ Assets/Scripts/Spawning.cs
@@ -17,18 +17,25 @@ public class Spawning : MonoBehaviour
     public static Spawning spwn;
 
 
-    public Transform collectablePrefab;
-    public Transform collectableSpawnPoint;
-    public float collectableSpawnDelay;
+    public GameObject collectablePrefab;        //prefab set in inspector
+    public Transform[] collectableSpawnPoint;   //spawnpoint array
+    public float collectableSpawnDelay;         //spawning delay
+
 
     public IEnumerator StartSpawn()
     {
-        yield return new WaitForSeconds(collectableSpawnDelay);
+        yield return new WaitForSeconds(collectableSpawnDelay); //spawn delay
 
-        Instantiate(collectablePrefab, collectableSpawnPoint.position, collectableSpawnPoint.rotation);
+        for (int i = 0; i < collectableSpawnPoint.Length; i++) //loop as many times as array length
+        {
+            int index = Random.Range(0, collectableSpawnPoint.Length); //Get random point on spawnpoint array
+            var spawnpoint = collectableSpawnPoint[index];
+            GameObject.Instantiate(collectablePrefab, spawnpoint.position, spawnpoint.rotation); //instantiate on spawnpoint
+        }
+
     }
 
-    public static void Spawn(Spawning spawn)
+    public static void Spawn(Spawning spawn) //public accessible void that starts spawning
     {
         spwn.StartCoroutine(spwn.StartSpawn());
     }
