7ac60fe73aebb6e871faa3785f24f30f523d2674
MiauwMaster <putm1500@student.nhl.nl>
2015-12-03 11:47:01 +0100

    Script voor exploderende blokken toegevoegd
    script ExplodingBlocks wat met een delay blokken laat verdwijnen

---
+++ Assets/Scripts/ExplodingBlock.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplodingBlock : MonoBehaviour {
+
+    public int destroyDelay;
+
+    IEnumerator OnCollisionEnter2D(Collision2D _colInfo)
+    {
+        Player _player = _colInfo.collider.GetComponent<Player>();
+        if (_player != null)
+        {
+            yield return new WaitForSeconds(destroyDelay); //destroy delay
+            Destroy(gameObject);    //destroy block
+        }
+    }
+}
