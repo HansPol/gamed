0c57e70f9c6ed94a99bf3e87428af8312fc6a7db
MiauwMaster <putm1500@student.nhl.nl>
2015-12-08 15:14:22 +0100

    prototype iteratie 2

--- Assets/Scripts/ExplodingBlock.cs
+++ Assets/Scripts/ExplodingBlock.cs
@@ -6,13 +6,17 @@ public class ExplodingBlock : MonoBehaviour
 
     public float destroyDelay;
 
+    public Transform destroyParticles;
+
     IEnumerator OnCollisionEnter2D(Collision2D _colInfo)
     {
         Player _player = _colInfo.collider.GetComponent<Player>();
         if (_player != null)
         {
             yield return new WaitForSeconds(destroyDelay); //destroy delay
+            Instantiate(destroyParticles, this.gameObject.transform.position, this.gameObject.transform.rotation);
             Destroy(gameObject);    //destroy block
+
         }
     }
 }
